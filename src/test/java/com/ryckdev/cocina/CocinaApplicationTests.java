21347bc0a67899b23f4d0dd22849225e8e2b05fa
RycardoMartynez <martynezrycardo@gmail.com>
2026-05-26 19:55:59 -0300

    Initial commit

---
+++ src/test/java/com/ryckdev/cocina/CocinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryckdev.cocina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
